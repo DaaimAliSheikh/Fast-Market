23262c09f1ced9d1f051d80e12697695abcc5395
daaimali <daaimalisheikh23@gmail.com>
2025-02-28 00:25:01 +0500

    added app.config.json

--- app.config.js
+++ app.config.js
@@ -1,6 +1,7 @@
-{
-  "expo": {
-    "name": "fast-market",
+export default ({ config }) => ({
+
+  ...config,
+  "name": "fast-market",
     "slug": "fast-market",
     "version": "1.0.0",
     "orientation": "portrait",
@@ -13,12 +14,24 @@
       "bundleIdentifier": "com.daaimali.fastmarket"
     },
     "android": {
+      ...config.android,
+      googleServicesFile: process.env.GOOGLE_SERVICES_JSON || './google-services.json',
       "adaptiveIcon": {
         "foregroundImage": "./assets/images/adaptive-icon.png",
         "backgroundColor": "#ffffff"
       },
       "package": "com.daaimali.fastmarket",
       "googleServicesFile": "./google-services.json"
+    },
+    "hooks": {
+      "postPublish": [
+        {
+          "file": "expo-constants/scripts/get-google-services-json.js",
+          "config": {
+            "base64": "$(GOOGLE_SERVICES_JSON)"
+          }
+        }
+      ]
     },
     "web": {
       "bundler": "metro",
@@ -49,5 +62,5 @@
         "projectId": "7d50b470-5711-4ad8-a9ec-bb3b4975b6b3"
       }
     }
-  }
-}
+  })
+
